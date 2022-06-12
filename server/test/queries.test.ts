b0389c01ec3be3c79e622d7616d3052dca47d64e
Quentin Moreau <quentinmoreauh@hotmail.com>
2022-06-12 11:25:15 +0200

    remove server queries test

--- server/test/queries.test.ts
+++
@@ -1,7 +0,0 @@
-import { deleteRecipeQuery } from '../shared/utils/queries'
-
-test('delete recipe with good id', async () => {
-    const id = '1'
-
-    expect(deleteRecipeQuery(id)).toContain(`WHERE r.idRecette = ${id}`)
-})
